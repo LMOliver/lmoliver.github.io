f1d74420625a4c2bfb9bcef5644bbae6e3ebe8e9
LMOliver <31990902+LMOliver@users.noreply.github.com>
2019-02-21 14:16:37 +0800

    Add files via upload

---
+++ blog/blog.js
@@ -0,0 +1,66 @@
+Vue.component('header-title',{
+	template:'<a href="#"><h1 :style="style"><slot></slot></h1></a>',
+	data:function(){
+		return {
+			style:{
+				display:'inline',
+				color: 'dodgerblue',
+				margin: 0,
+			},
+		}
+	},
+});
+Vue.component('side-bar',{
+	template:'<div :style="style"><slot></slot><div>',
+	props:['pos'],
+	data:function(){
+		return {
+			style:{
+				width:'10%',
+				float:this.pos,
+			},
+		};
+	},
+});
+Vue.component('blog-context',{
+	template:'<div :style="style" v-html="context"><div>',
+	props:['href'],
+	data:function(){
+		return {
+			style:{
+				width:'80%',
+				float:'left',
+			},
+			context:'',
+		};
+	},
+	watch:{
+		href(href){
+			this.context='<p>Loading...</p>';
+			console.log(href);
+			axios.get(href?`./${href}/post.md`:'./index.md')
+				.then(function(result){
+					console.log(result);
+				})
+				.catch(function(reason){
+					console.log(reason);
+				});
+			console.log(`href => ${href}`);
+		}
+	}
+});
+var data={
+	blogHref:null,
+};
+function getBlogHref(){
+	var c=window.location.hash;
+	return c[0]==='#'?c.slice(1):c;
+}
+window.onhashchange=function(){
+	data.blogHref=getBlogHref();
+}
+var app = new Vue({
+	el: '#app',
+	data,
+});
+data.blogHref=getBlogHref();
