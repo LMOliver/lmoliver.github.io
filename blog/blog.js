c795d5e5973962cbed9ec320061b68302738b690
LMOliver <31990902+LMOliver@users.noreply.github.com>
2019-02-21 18:57:03 +0800

    %%%ZZD

--- blog/blog.js
+++ blog/blog.js
@@ -1,5 +1,5 @@
 Vue.component('header-title',{
-	template:'<a href="#"><h1 :style="style"><slot></slot></h1></a>',
+	template:'<a href="#!"><h1 :style="style"><slot></slot></h1></a>',
 	data:function(){
 		return {
 			style:{
@@ -10,8 +10,9 @@ Vue.component('header-title',{
 		}
 	},
 });
+
 Vue.component('side-bar',{
-	template:'<div :style="style"><slot></slot><div>',
+	template:'<aside :style="style"><slot></slot></aside>',
 	props:['pos'],
 	data:function(){
 		return {
@@ -22,45 +23,141 @@ Vue.component('side-bar',{
 		};
 	},
 });
+
 Vue.component('blog-context',{
-	template:'<div :style="style" v-html="context"><div>',
+	template:'<article :style="style"><div></div></article>',
 	props:['href'],
 	data:function(){
 		return {
 			style:{
-				width:'80%',
-				float:'left',
+				width:'70%',
+				position:'relative',
+				left:'15%',
+				border:'2px solid black',
+				'border-radius':'5px',
+				padding:'10px',
 			},
 			context:'',
+			vue:null,
 		};
 	},
 	watch:{
 		href(href){
 			this.context='<p>Loading...</p>';
-			console.log(href);
-			axios.get(href?`./${href}/post.md`:'./index.md')
-				.then(function(result){
-					this.context=marked(result);
-				})
-				.catch(function(reason){
-					this.context=`<h1>GG!</h1><p>reason:${reason}</p>`;
-				});
+			var vm=this;
+			axios({
+				method:'get',
+				url:href?`./${href}/post.md`:'./index.md',
+				responseType:'text',
+			})
+			.then(function(result){
+				vm.context=marked(result.data);
+			})
+			.catch(function(...reason){
+				vm.context=`<h1>GG!</h1><p>${reason}</p><a href="#!">back</a>`;
+			});
 			console.log(`href => ${href}`);
-		}
+		},
+		context(text){
+			var res=Vue.compile('<div>'+text+'</div>');
+			if(this.vue)this.vue.$destroy();
+			this.vue=new Vue({
+				el:this.$el.children[0],
+				render:res.render,
+				staticRenderFns:res.staticRenderFns,
+				parent:this,
+			})
+		},
 	}
 });
-var data={
-	blogHref:null,
-};
-function getBlogHref(){
-	var c=window.location.hash;
-	return c[0]==='#'?c.slice(1):c;
-}
-window.onhashchange=function(){
-	data.blogHref=getBlogHref();
-}
+
+Vue.component('demo-button',{
+	template:`
+		<div style="display:inline;">
+			<button @click="moZZD" style="color:red">{{moText}}</button>
+			<span>{{moValueText}}</span>
+			<div>
+				<button @click="buyAdvancedMo" v-if="moCount>=10"
+				:disabled="moValue<advancedMoCost">{{advancedMoText}}</button>
+			</div>
+			<div>
+				<button @click="buyMoer" v-if="advancedMoLevel>=10"
+				:disabled="moValue<moerCost">{{moerText}}</button>
+			</div>
+			<div>
+				<button @click="buyChurch" v-if="advancedMoLevel>=20"
+				:disabled="moers<churchCost">{{churchText}}</button>
+			</div>
+		</div>`,
+	methods:{
+		moZZD(){
+			this.moCount+=1;
+			this.moValue+=this.moDelta;
+		},
+		buyAdvancedMo(){
+			this.moValue-=this.advancedMoCost;
+			this.advancedMoLevel+=1;
+		},
+		buyMoer(){
+			this.moValue-=this.moerCost;
+			this.moers+=1;
+		},
+		buyChurch(){
+			this.moers-=this.churchCost;
+			this.churchs+=1;
+		},
+	},
+	computed:{
+		moDelta(){
+			return (1+this.advancedMoLevel)*(1+this.moers);
+		},
+		moText(){
+			return '膜拜ZZD'+(this.moDelta>1?(this.moDelta+'次'):'');
+		},
+		moValueText(){
+			return this.moValue>0?('您已膜拜'+this.moValue+'次'):'';
+		},
+		advancedMoText(){
+			return `真诚膜拜${this.advancedMoLevel>0?`Lv.${this.advancedMoLevel}`:''} [${this.advancedMoCost}次膜拜]`;
+		},
+		advancedMoCost(){
+			return Math.floor(10*Math.pow(1.3,this.advancedMoLevel)/Math.pow(4,this.churchs));
+		},
+		moerText(){
+			return `信徒${this.moers>0?`*${this.moers}`:''} [${this.moerCost}次膜拜]`;
+		},
+		moerCost(){
+			return Math.floor(100*Math.pow(1.8,this.moers));
+		},
+		churchText(){
+			return `教堂${this.churchs>0?`*${this.churchs}`:''} [${this.churchCost}位信徒]`;
+		},
+		churchCost(){
+			return Math.floor(5+Math.pow(this.churchs,1.2));
+		},
+	},
+	data:function(){
+		return {
+			moCount:0,
+			moValue:0,
+			advancedMoLevel:0,
+			moers:0,
+			churchs:0,
+		};
+	},
+});
+
 var app = new Vue({
 	el: '#app',
-	data,
+	data:{
+		blogHref:null,
+	},
 });
-data.blogHref=getBlogHref();
+function getBlogHref(){
+	var c=window.location.hash;
+	return c.indexOf('#!')===0?c.slice(2):c;
+}
+window.onhashchange=function(){
+	app.blogHref=getBlogHref();
+}
+app.blogHref=getBlogHref();
--- blog/index.md
+++ blog/index.md
@@ -1,3 +1,19 @@
-# Test
+## Test
 
-QAQ
+----
+
+- ![ZZDAKIOI](https://images2018.cnblogs.com/blog/1209138/201809/1209138-20180902190737450-1861120566.png)
+- [QYAKIOI](#!1)
+- S<span style="color:red">TEPS</span>AKIOI
+
+1. **QAQ**
+2. _qwq_
+3. <demo-button></demo-button>
+
+![](https://images0.cnblogs.com/blog/163402/201402/070901493627278.png)
+
+|Q|A|Q|A|Q|
+|-|-|-|-|-|
+|T|h|i|s| |
+|i|s| |a| |
+|T|A|B|L|E|
